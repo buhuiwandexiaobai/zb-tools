675980d06bf742f5aad167da83482520b96f1cf9
zhaobin <zhaobin@kuaishou.com>
2021-06-17 22:54:40 +0800

    add array utils

---
+++ src/main/java/io/github/buhuiwandexiaobai/list/ArrayUtils.java
@@ -0,0 +1,13 @@
+package io.github.buhuiwandexiaobai.list;
+
+import java.util.Arrays;
+
+/**
+ * @author zhaobin
+ * @date 2021/6/17 22:53
+ */
+public class ArrayUtils {
+    public static void print(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,6 @@
+/**
+ * @author zhaobin
+ * @date 2021/6/17 22:52
+ */
+public class TestTest {
+}
